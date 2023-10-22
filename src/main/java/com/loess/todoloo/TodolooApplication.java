1d60f88237011aafe1977201665315deffa6f98b
BlacK <smartwiz@mail.ru>
2023-10-22 17:05:46 +0300

    innit spring app

---
+++ src/main/java/com/loess/todoloo/TodolooApplication.java
@@ -0,0 +1,13 @@
+package com.loess.todoloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loess/todoloo/TodoolooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loess.todoloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
