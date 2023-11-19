a3e79b5f8ca3a0fb0ae1c951fc4ad618e169af04
BlacK <smartwiz@mail.ru>
2023-11-19 14:43:59 +0300

    cleanup TaskRepo, FamilyRepo

--- src/main/java/com/loess/todoloo/model/db/repository/FamilyRepo.java
+++ src/main/java/com/loess/todoloo/model/db/repository/FamilyRepo.java
@@ -1,14 +1,10 @@
 package com.loess.todoloo.model.db.repository;
 
 import com.loess.todoloo.model.db.entity.Family;
-import com.loess.todoloo.model.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface FamilyRepo extends JpaRepository<Family, Long> {
 
-//    Optional<Family> findByEmail(String email);
 }
--- src/main/java/com/loess/todoloo/model/db/repository/TaskRepo.java
+++ src/main/java/com/loess/todoloo/model/db/repository/TaskRepo.java
@@ -1,11 +1,8 @@
 package com.loess.todoloo.model.db.repository;
 
 import com.loess.todoloo.model.db.entity.Task;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,13 +10,6 @@ import java.util.List;
 @Repository
 public interface TaskRepo extends JpaRepository<Task, Long> {
 
-    List<Task> findAllByAssigneeId(Long assigneeId);
     List<Task> findAllByAssigneeId(Long assigneeId, Sort sort);
-    List<Task> findAllByAuthorId(Long authorId);
 
-//    @Query("select c from Car c where c.status <> '2'")
-//    List<Car> findAllNotDeleted(Pageable request);
-//
-//    @Query("select c from Car c where c.status <> '2' and  (c.brand like %:filter% or c.model like %:filter%)")
-//    List<Car> findAllNotDeleted(Pageable request, @Param("filter") String filter);
 }
